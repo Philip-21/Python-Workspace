8c1b665316e9f6c19bcb8625719900b8bd01324e
Philip-21 <philipuzomaobiora@gmail.com>
2023-01-31 07:29:15 +0100

    finishing school project

--- school-project/test.py
+++ school-project/test.py
@@ -9,7 +9,16 @@ def twos_compliment(n,bit_len):
             # If the input number is negative, it first calculates the 2's complement by adding 2^n, 
             # where n is the number of bits and then convert it to binary.
             return format(2**bit_len + n,'b').zfill(bit_len)
-ent1 = int(input("Enter 1st value:"))
-ent2 = int(input("enter value:"))
-total = ent1 +ent2
-print(twos_compliment(total, 4))
+
+try:
+    ent1 = float(input("Enter 1st value:"))
+    ent2 = float(input("Enter 2nd value:"))
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
+else:
+    # Round the float values to a specified number of decimal places
+    ent1 = int(round(ent1))
+    ent2 = int(round(ent2))
+
+    total = ent1 + ent2
+    print(twos_compliment(total, 4))
